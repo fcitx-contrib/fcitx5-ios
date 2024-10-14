2e383213b0e6dbed2c75da1807603b6782c1beda
Qijia Liu <liumeo@pku.edu.cn>
2024-10-13 23:53:45 -0400

    show Documents directory in Files.app

--- src/ContentView.swift
+++ src/ContentView.swift
@@ -9,5 +9,11 @@ struct ContentView: View {
       Text("Hello, world!")
     }
     .padding()
+    .onAppear {
+      // The stupid iOS doesn't show empty directory in Files.app.
+      try? "".write(
+        to: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+          .appendingPathComponent("placeholder"), atomically: true, encoding: .utf8)
+    }
   }
 }
