1ac33caf7699983e61ad40fc42137e7e57b021bc
Qijia Liu <liumeo@pku.edu.cn>
2024-10-19 22:17:06 -0400

    configure app group; change data&config directory (#14)

--- keyboard/fcitx.h
+++ keyboard/fcitx.h
@@ -2,7 +2,7 @@
 
 #include <objc/objc.h>
 
-void startFcitx(const char *bundlePath);
+void startFcitx(const char *bundlePath, const char *appGroupPath);
 void focusIn(id client);
 void focusOut();
 bool processKey(const char *key);
