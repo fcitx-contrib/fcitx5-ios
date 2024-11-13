490c55a710534ecf8c6875b3961d39a5d4cccf29
Qijia Liu <liumeo@pku.edu.cn>
2024-11-12 19:38:31 -0500

    simplify cmake (#24)

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -29,3 +29,13 @@ done
 sed -i '' "s|=/usr/include|=$(pwd)/$EXTRACT_DIR/include|" $EXTRACT_DIR/lib/pkgconfig/{json-c,libuv,marisa,rime}.pc
 sed -i '' "s|-L\${libdir} -luv|$(pwd)/$EXTRACT_DIR/lib/libuv.a|" $EXTRACT_DIR/lib/pkgconfig/libuv.pc
 sed -E -i '' "s|Libs:.*-l(.*)|Libs: $(pwd)/$EXTRACT_DIR/lib/lib\1.a|" $EXTRACT_DIR/lib/pkgconfig/json-c.pc
+
+plugins=(
+  rime
+)
+
+for plugin in "${plugins[@]}"; do
+  file=$plugin-any.tar.bz2
+  [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-macos-plugins/releases/download/latest/$file
+  tar xjvf cache/$file -C $EXTRACT_DIR
+done
