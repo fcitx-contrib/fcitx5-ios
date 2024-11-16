8975a9223c707356518a8fd23430681e84aa9b04
Qijia Liu <liumeo@pku.edu.cn>
2024-11-16 00:14:35 -0500

    fake find fcitx5 packages in engines to reduce patch

---
+++ cmake/FindFcitx5Module.cmake
@@ -0,0 +1,1 @@
+set(Fcitx5Module_FOUND TRUE)
---
+++ cmake/FindFcitx5Utils.cmake
@@ -0,0 +1,6 @@
+set(Fcitx5Utils_FOUND TRUE)
+
+# dependent projects usually use
+# "${FCITX_INSTALL_CMAKECONFIG_DIR}/Fcitx5Utils/Fcitx5CompilerSettings.cmake"
+# to locate Fcitx5CompilerSettings
+set(FCITX_INSTALL_CMAKECONFIG_DIR "${CMAKE_CURRENT_LIST_DIR}")
