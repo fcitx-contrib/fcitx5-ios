b568289242ef42859dc59217d525710ce1691606
Qijia Liu <liumeo@pku.edu.cn>
2024-12-14 23:30:15 -0500

    fix editing TextField causes re-render

--- src/config/StringView.swift
+++ src/config/StringView.swift
@@ -4,26 +4,28 @@ struct StringView: View, OptionViewProtocol {
   let label: String
   let data: [String: Any]
   @Binding var value: Any
+  @State private var text: String = ""
+  @FocusState private var isFocused: Bool
 
   var body: some View {
     HStack {
       if !label.isEmpty {
         Text(label)
       }
-      TextField(
-        "",
-        text: Binding<String>(
-          get: { value as! String },
-          set: {
-            // Avoid unnecessary write.
-            if value as! String != $0 {
-              value = $0
-            }
+      TextField("", text: $text)
+        .focused($isFocused)
+        // Don't update real-time. It changes parent state so the whole view is re-rendered.
+        // Don't use onSubmit. It requires Enter key to be pressed.
+        .onChange(of: isFocused) {
+          // Avoid unnecessary write.
+          if !isFocused && value as! String != text {
+            value = text
           }
-        )
-      )
-      // Leading for List item, trailing for String option.
-      .multilineTextAlignment(label.isEmpty ? .leading : .trailing)
+        }
+        // Leading for List item, trailing for String option.
+        .multilineTextAlignment(label.isEmpty ? .leading : .trailing)
+    }.onAppear {
+      text = value as! String
     }
   }
 }
