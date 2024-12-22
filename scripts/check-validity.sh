4aa7b8b647ec0011e0d42af37bcdc5da3ebd3185
Qijia Liu <liumeo@pku.edu.cn>
2024-12-22 10:11:49 -0500

    drop libime patch

--- scripts/check-validity.sh
+++ scripts/check-validity.sh
@@ -2,10 +2,15 @@ set -e
 
 has_dl=0
 
-executable=build/src/Release-iphonesimulator/Fcitx5.app/PlugIns/keyboard.appex/keyboard
+executables=(
+  build/src/Release-iphonesimulator/Fcitx5.app/Fcitx5
+  build/src/Release-iphonesimulator/Fcitx5.app/PlugIns/keyboard.appex/keyboard
+)
 
-if nm $executable | grep dlopen; then
-  has_dl=1
-fi
+for executable in "${executables[@]}"; do  
+  if nm $executable | grep dlopen; then
+    has_dl=1
+  fi
+done
 
 exit $has_dl
