8ac4f8ac01833f343694b1cab14b822aa4c54c3e
Inoki <veyx.shaw@gmail.com>
2024-12-29 11:05:58 +0100

    Improve symbol and alphabet key actions

--- uipanel/KeyAction.swift
+++ uipanel/KeyAction.swift
@@ -2,11 +2,29 @@ import FcitxProtocol
 
 class KeyAction {
   var key: String
+  var capsKey: String
+  var label: String
+  var capsLabel: String
   var action: (_ client: FcitxProtocol) -> Void
 
-  init(key: String, action: @escaping (_ client: FcitxProtocol) -> Void) {
+  init(key: String, capsKey: String, label: String, capsLabel: String, action: @escaping (_ client: FcitxProtocol) -> Void) {
     self.key = key
+    self.capsKey = capsKey
+    self.label = label
+    self.capsLabel = capsLabel
     self.action = action
+  }
+
+  convenience init(key: String, capsKey: String, action: @escaping (_ client: FcitxProtocol) -> Void) {
+    self.init(key: key, capsKey: capsKey, label: key, capsLabel: capsKey, action: action)
+  }
+
+  convenience init(key: String, label: String, action: @escaping (_ client: FcitxProtocol) -> Void) {
+    self.init(key: key, capsKey: key, label: label, capsLabel: label, action: action)
+  }
+
+  convenience init(key: String, action: @escaping (_ client: FcitxProtocol) -> Void) {
+    self.init(key: key, capsKey: key, action: action)
   }
 
   func perform(_ client: FcitxProtocol) {
@@ -18,6 +36,9 @@ class AlphabetKeyAction: KeyAction {
   init(key: String) {
     super.init(
       key: key,
+      capsKey: key.uppercased(),
+      label: key,
+      capsLabel: key.uppercased(),
       action: { client in
         client.keyPressed(key)
       })
@@ -25,10 +46,13 @@ class AlphabetKeyAction: KeyAction {
 }
 
 class SymbolKeyAction: KeyAction {
-  init(symbol: String) {
+  init(symbol: String, label: String) {
     // Use the symbol name of keyNameList in `fcitx5/src/lib/fcitx-utils/keynametable.h`
     super.init(
       key: symbol,
+      capsKey: symbol,
+      label: label,
+      capsLabel: label,
       action: { client in
         client.keyPressed(symbol)
       })
@@ -37,6 +61,6 @@ class SymbolKeyAction: KeyAction {
 
 class BackspaceKeyAction: SymbolKeyAction {
   init() {
-    super.init(symbol: "BackSpace")
+    super.init(symbol: "BackSpace", label: "⌫")
   }
 }
