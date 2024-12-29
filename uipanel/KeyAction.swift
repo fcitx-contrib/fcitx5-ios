6424a796686f0585dd959b9ac09b6efaddf4fe65
Inoki <veyx.shaw@gmail.com>
2024-12-29 10:42:02 +0100

    Create a symbol key action

--- uipanel/KeyAction.swift
+++ uipanel/KeyAction.swift
@@ -1,32 +1,42 @@
 import FcitxProtocol
 
 class KeyAction {
-    var key: String
-    var action: (_ client: FcitxProtocol) -> Void
+  var key: String
+  var action: (_ client: FcitxProtocol) -> Void
 
-    init(key: String, action: @escaping (_ client: FcitxProtocol) -> Void) {
-        self.key = key
-        self.action = action
-    }
+  init(key: String, action: @escaping (_ client: FcitxProtocol) -> Void) {
+    self.key = key
+    self.action = action
+  }
 
-    func perform(_ client: FcitxProtocol) {
-        self.action(client)
-    }
+  func perform(_ client: FcitxProtocol) {
+    self.action(client)
+  }
 }
 
 class AlphabetKeyAction: KeyAction {
-    init(key: String) {
-        super.init(key: key, action:  { client in
-            client.keyPressed(key)
-        })
-    }
+  init(key: String) {
+    super.init(
+      key: key,
+      action: { client in
+        client.keyPressed(key)
+      })
+  }
 }
 
-class BackspaceKeyAction: KeyAction {
-    init() {
-        super.init(key: "Backspace", action: { client in
-            // Use the symbol name of keyNameList in `fcitx5/src/lib/fcitx-utils/keynametable.h`
-            client.keyPressed("BackSpace")
-        })
-    }
+class SymbolKeyAction: KeyAction {
+  init(symbol: String) {
+    // Use the symbol name of keyNameList in `fcitx5/src/lib/fcitx-utils/keynametable.h`
+    super.init(
+      key: symbol,
+      action: { client in
+        client.keyPressed(symbol)
+      })
+  }
+}
+
+class BackspaceKeyAction: SymbolKeyAction {
+  init() {
+    super.init(symbol: "BackSpace")
+  }
 }
