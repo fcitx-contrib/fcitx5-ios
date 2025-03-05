9b2153401793fb5a50792731a3416dab4201b923
Qijia Liu <liumeo@pku.edu.cn>
2025-03-04 22:00:03 -0500

    use C++20

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -29,12 +29,12 @@ fi
 for dep in "${deps[@]}"; do
   file=$dep$POSTFIX.tar.bz2
   [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-prebuilder/releases/download/ios/$file
-  tar xjvf cache/$file -C $EXTRACT_DIR
+  tar xf cache/$file -C $EXTRACT_DIR
 done
 
 file=Fcitx5-arm64.tar.bz2
 [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-macos/releases/download/latest/$file
-tar xjvf cache/$file -C $SPELL_DICT_DIR --strip-components=5 Fcitx5.app/Contents/share/fcitx5/spell/en_dict.fscd
+tar xf cache/$file -C $SPELL_DICT_DIR --strip-components=5 Fcitx5.app/Contents/share/fcitx5/spell/en_dict.fscd
 
 plugins=(
   chinese-addons
@@ -43,6 +43,6 @@ plugins=(
 
 for plugin in "${plugins[@]}"; do
   file=$plugin-any.tar.bz2
-  [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/macos/$file
-  tar xjvf cache/$file -C $EXTRACT_DIR
+  [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/macos-latest/$file
+  tar xf cache/$file -C $EXTRACT_DIR
 done
