14bda67509fa16f97cf4db8fb17363c5a89536a9
Qijia Liu <liumeo@pku.edu.cn>
2025-12-18 19:09:53 -0500

    task isolation for notification

--- iosnotifications/notify.swift
+++ iosnotifications/notify.swift
@@ -1,6 +1,8 @@
 public func showTip(_ icon: String, _ body: String, _ timeout: Int32) {
-  guard let showToastCallback = showToastCallback else {
-    return
+  Task { @MainActor in
+    guard let showToastCallback = showToastCallback else {
+      return
+    }
+    showToastCallback(icon, body, timeout)
   }
-  showToastCallback(icon, body, timeout)
 }
