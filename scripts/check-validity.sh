c73edc603b2c91b64cd1bc9d5d0a1c364fd07065
Qijia Liu <liumeo@pku.edu.cn>
2026-04-25 14:40:43 -0400

    fixup

--- scripts/check-validity.sh
+++ scripts/check-validity.sh
@@ -4,22 +4,17 @@ platform=$1
 
 has_dl=0
 
-if [[ "$platform" == "OS64" ]]; then
-  postfix=os
-else
-  postfix=simulator
-fi
-
 executables=(
-  build/$1/src/Release-iphone$postfix/Fcitx5.app/Fcitx5
-  build/$1/src/Release-iphone$postfix/Fcitx5.app/PlugIns/Chinese.appex/Chinese
-  build/$1/src/Release-iphone$postfix/Fcitx5.app/PlugIns/Hallelujah.appex/Hallelujah
-  build/$1/src/Release-iphone$postfix/Fcitx5.app/PlugIns/Mozc.appex/Mozc
-  build/$1/src/Release-iphone$postfix/Fcitx5.app/PlugIns/Rime.appex/Rime
+  build/$1/src/Fcitx5.app/Fcitx5
+  build/$1/src/Fcitx5.app/PlugIns/Chinese.appex/Chinese
+  build/$1/src/Fcitx5.app/PlugIns/Hallelujah.appex/Hallelujah
+  build/$1/src/Fcitx5.app/PlugIns/Mozc.appex/Mozc
+  build/$1/src/Fcitx5.app/PlugIns/Rime.appex/Rime
 )
 
 for executable in "${executables[@]}"; do  
-  if nm $executable | grep dlopen; then
+  output=$(nm "$executable")
+  if echo "$output" | grep dlopen; then
     has_dl=1
   fi
 done
